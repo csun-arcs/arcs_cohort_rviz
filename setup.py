00833f8cd77d39fbc61b28993ad7a49c64dc3f3a
Barry Ridge <barry.ridge@csun.edu>
2025-05-05 13:35:56 -0700

    Add missing files to setup

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        ('share/' + package_name + '/launch', [
+            'launch/rviz.launch.py']),
+        ('share/' + package_name + '/rviz', [
+            'rviz/cohort_default.rviz']),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
